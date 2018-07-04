da3ea522949f3537614913b3f0426b23f56be4ec
Anita Beauchamp <amadamspr@gmail.com>
2018-07-04 06:39:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(arg)
+def display_rainbow(colors)
   #puts"R: #{arg[0]}, O: #{arg[0]}, Y: #{arg[0]}, G: #{arg[0]}, B: #{arg[0]}, I: #{arg[0]}, V: #{arg[0]}"
   puts"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
